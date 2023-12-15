874b997033028e576759090670ac81661c58f8e2
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-15 15:27:30 +0530

    still contuning

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,9 +4,10 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-   echo "ERROR: please run this script with root access"
+    echo "ERROR: please run this script with root access"
+    exit 1 # you can give other than 0
 else
-   echo "you are root user"
+    echo "you are root user"
 fi
 
 yum install mysql -y
