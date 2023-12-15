f50d004d16a505c6f467749d94930465c506e5a5
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-15 21:01:05 +0530

    part of debug process

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,18 +3,18 @@
 ID=$(id -u)
 
 VALIDATE(){
-    if [ $? -ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo "ERROR: Installing is failed"
+        echo "ERROR:: $2 ..failed"
         exit 1
     else
-    echo "ERROR: Installing is success"
+    echo "$2...success"
     fi    
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR: please run this script with root access"
+    echo "ERROR:: please run this script with root access"
     exit 1 # you can give other than 0
 else
     echo "you are root user"
@@ -22,8 +22,9 @@ fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y
 
-VALIDATE
+VALIDATE $? " installing mysql "
 
 yum install git -y
 
-VALIDATE
+VALIDATE $? " installing git "
+
