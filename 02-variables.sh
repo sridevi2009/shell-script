82cf6fa85670b478393cfbc2760ed6bc246880fe
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-15 12:29:49 +0530

    still contuning

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- PERSON1 = RAMESH 
- PERSON2 = SURESH
+ PERSON1 = RAM
+ PERSON2 = RAHEEM
 
-echo "$PERSON1 : hi $PERSON2 good morning"
-echo "$PERSON2 : hi $PERSON1 very good morning"
+echo "$PERSON1 : hi $PERSON2, good morning"
+echo "$PERSON2 : hi $PERSON1, very good morning"
 echo "$PERSON1 : hw r u $PERSON2"
 echo "$PERSON2 : im fine thnak you"
