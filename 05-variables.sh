69a0cf9b368c0c38a309abc106d4b920833a3cfa
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-15 14:04:43 +0530

    still contuning

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
   
  echo "please enter your username"
- read USERNAME #the value entered above will be automatically attached to USERNAME variable
+ read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
-USERNAME=$1
-PASSWORD=$2
 
-echo "username is: $USERNAME, password is: $PASSWORD"
+echo "username is: $USERNAME,# im printing just for validation
