adae661f333e5bdd6a60a69305db5c44a5f73ee6
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-15 20:27:40 +0530

    part of debug process

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -19,3 +19,13 @@ then
 else
    echo "ERROR: Installing mysql is success"
 fi    
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR: Installing git is failed"
+    exit 1
+else
+   echo "ERROR: Installing git is success"
+fi   
