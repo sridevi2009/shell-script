f10481914c29ecf7b045144cbffb9a25664d4ef1
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-15 13:49:23 +0530

    still contuning

---
+++ 04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ PERSON1 = $1
+ PERSON2 = $2
+
+echo "$PERSON1 : hi $PERSON2, good morning"
+echo "$PERSON2 : hi $PERSON1, very good morning"
+echo "$PERSON1 : hw r u $PERSON2, wt about u"
+echo "$PERSON2 : im fine thnak you $PERSON1"
