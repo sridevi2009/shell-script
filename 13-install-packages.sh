b5205706d844e9e0e3823c3fc221165d9e5f3b38
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-16 13:37:53 +0530

    as a part of bugh process

---
+++ 13-install-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end
+
+echo "all arguments passed: $@"
