dd7b02790f5b94fc59ea3e7b7d4476bd44fbc4cb
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-16 14:10:07 +0530

    as a part of bugh process

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 ID=$(id -u)
 R="\e[31m"
-g="\e[32m"
+G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then 
+        echo -e "$2..$R failed $N"
+    else
+        echo -e "$2..$G success $N"
+    fi    
+}        
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: please run this script with root access $N"
@@ -12,4 +26,17 @@ else
     echo "you are root user"
 fi # fi means reverse of if, indicating condition end
 
-echo "all arguments passed: $@"
+#echo "all arguments passed: $@"
+#git mysql postfix net-tools
+#package=git for first time
+for package in $@
+do
+   yum list installed $package  &>> $LOGFILE #check installed or not
+   if [$? -ne 0] #if not installed
+   then
+       yum install $package -y &>> $LOGFILE # install the package
+       VALIDATE $? " installation of $package" #validate
+    else   
+        echo -e "$package is already installed..$Y skipping $N"
+    fi    
+   done   
