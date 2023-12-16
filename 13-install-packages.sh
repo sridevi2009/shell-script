d588f647d3089746f8cf2806ae1472a9c13d7ad2
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-16 14:16:45 +0530

    as a part of bugh process

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -13,9 +13,9 @@ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
     if[ $1 -ne 0 ]
     then 
-        echo -e "$2..$R FAILED $N"
+        echo -e " $2..$R FAILED $N "
     else
-        echo -e "$2..$G SUCCESS $N"
+        echo -e " $2..$G SUCCESS $N "
     fi    
 }        
 if [ $ID -ne 0 ]
