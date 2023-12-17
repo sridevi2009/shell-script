7fe4eb579000d92843dadeac345b6f9a0d1876b9
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-17 12:03:06 +0530

    roboshop shell script

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,30 +2,37 @@
 
  
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R=\e[31m"
+G=\e[32m"
+N=\e[0m"
 
-echo "script name: $0"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 ..failed"
+        echo -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-    echo "$2...success"
+         echo -e "$2 ... $G SUCCESS $N"
     fi    
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: please run this script with root access"
+    echo -e "$R ERROR:: please run this script with root access $N"
     exit 1 # you can give other than 0
 else
     echo "you are root user"
 fi # fi means reverse of if, indicating condition end
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? " installing mysql "
 
-yum install git -y
+yum install git -y  $LOGFILE
 
 VALIDATE $? " installing git "
