0f5865526b57d829b84170d6ac651b62ec473b1c
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-18 14:31:38 +0530

    roboshop shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,7 @@
 PERSON1= "RAM"
 PERSON2= "RAHEEM"
 
-echo "$PERSON1 : hi $PERSON2, good morning"
-echo "$PERSON2 : hi $PERSON1, very good morning"
-echo "$PERSON1 : hw r u $PERSON2, wt about u"
-echo "$PERSON2 : im fine thnak you $PERSON1"
+echo "$PERSON1: hi $PERSON2, good morning"
+echo "$PERSON2: hi $PERSON1, very good morning"
+echo "$PERSON1: hw r u $PERSON2, wt about u"
+echo "$PERSON2: im fine thnak you $PERSON1"
