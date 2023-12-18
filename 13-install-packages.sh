82e597c5baae7c3c94bda0b5806115074ea75b42
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-18 20:53:54 +0530

    roboshop shell script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-   echo -e"$R ERROR: please run this script with root access $N"
+   echo -e "$R ERROR: please run this script with root access $N"
    exit 1 #you ca give other than 0
 else
    echo "you are root user"
