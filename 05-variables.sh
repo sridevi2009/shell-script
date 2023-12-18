9d50ea58245515106b97ca337f326d31896f1b94
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-18 15:06:06 +0530

    just a part of debugh process

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
  echo "please enter your username"
- read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
+ read -s abc #the value entered above will be automatically attached to USERNAME variable
 
 
-echo "username is: $USERNAME" # im printing just for validation
+echo "username is: $abc" # im printing just for validation
