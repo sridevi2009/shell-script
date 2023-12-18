003d0d24fa8f9917243b6b214a6fef296ba8fc5e
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-18 14:53:05 +0530

    just a part of debugh process

--- 04.variables.sh
+++ 04.variables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
  PERSON1=$1
  PERSON2=$2
+ PERSON3=$3
+ PERSON4=$4
 
 echo "$PERSON1 : hi $PERSON2, good morning"
 echo "$PERSON2 : hi $PERSON1, very good morning"
 echo "$PERSON1 : hw r u $PERSON2, wt about u"
 echo "$PERSON2 : im fine thnak you $PERSON1"
+echo "$PERSON3 : im fine thnak you $PERSON4"
+echo "$PERSON4 : im fine thnak you $PERSON3"
