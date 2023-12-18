adfeab12bd3f88e0cb1fa205092b5c174a73f7c6
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-18 21:15:42 +0530

    roboshop shell script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -4,6 +4,20 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2 ... $R FAILED $N"
+    else
+       echo -e "$2 ... $G success $N"   
+    fi
+}
+
 if [ $ID -ne 0 ]
 then
    echo -e "$R ERROR: please run this script with root access $N"
@@ -12,4 +26,18 @@ else
    echo "you are root user"
 fi
 
-echo "all arguments passed: $@"
+#echo "all arguments passed: $@"
+# git mysql gcc net-tools postfix
+# package git for first time
+
+for package in $@ 
+do
+   yum list installed $package &>> $LOGFILE
+   if [ $? -ne 0 ]
+   then
+      yum install $package -y &>> $LOGFILE
+      VALIDATE $? " installation of $package"
+    else
+       echo -e "$package is already installed ... $Y skipping $N"
+    fi     
+done      
