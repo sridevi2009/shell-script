0d33d916eb54d134b47e9deb18668467a4fc5374
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-20 22:29:50 +0530

    roboshop shell script

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 NAME=""
-WISHES=""
+WISHES=" GOOD MORNING "
 
 USAGE(){
     echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "options::"
     echo " -n, specify the name (mandatory)"
-    echo " -w, specify the wishes. ex, good morning"
+    echo " -w, specify the wishes. (optional). default is good morning"
     echo " -h, display help and exit"
 }
 while getopts ":n:w:h" opt; do
@@ -21,8 +21,9 @@ while getopts ":n:w:h" opt; do
     esac 
 done
 
-if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-   echo "ERROR: both -n and -w are mandatory options."
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+if [ -z "$NAME" ]; then
+   echo "ERROR:  -n is mandatory."
    USAGE
    exit 1
 fi
