f7bd439cda91fbae02df650922f2dc7dd6e48d59
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-20 12:35:45 +0530

    roboshop shell script

--- 15-delete-old-logfiles.sh
+++ 15-delete-old-logfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOURCE_DIR=/tmp/shellscript-logs
+SOURCE_DIR="/tmp/shellscript-logs"
 
 R="\e[31m"
 G="\e[32m"
@@ -16,6 +16,6 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
- echo "deleting file: $line"
+  echo "deleting file: $line"
 done <<< $FILES_TO_DELETE
- 
+
