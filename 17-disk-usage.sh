e09ad1dbce0c7e20f2c806aaf57d33dc1ee539c1
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-20 14:46:44 +0530

    roboshop shell script

---
+++ 17-disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|file')
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do
+   usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+   partion=$(echo $line | awk '{print $1F}')
+   if [ $usage -gt $DISK_THRESHOLD ]
+   then
+       message+="high disk usahe on $partition: $usage"
+    fi   
+done <<< $DISK_USAGE
+
+echo "message: $message"
