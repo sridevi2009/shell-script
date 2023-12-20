675f67d539b0f45608ceaf0fd643c423020e17d7
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-20 14:51:53 +0530

    roboshop shell script

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -7,7 +7,7 @@ message=""
 while IFS= read line
 do
    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-   partion=$(echo $line | awk '{print $1F}')
+   partition=$(echo $line | awk '{print $1F}')
    if [ $usage -ge $DISK_THRESHOLD ]
    then
        message="high disk usage on $partition: $usage"
