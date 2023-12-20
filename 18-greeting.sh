70a86519bfcb538dbf1e3ff0e569541c0a6f4f0c
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-20 22:23:35 +0530

    roboshop shell script

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -15,7 +15,16 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
         h|*) USAGE; exit;;
         :) USAGE; exit;;
     esac 
 done
+
+if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+   echo "ERROR: both -n and -w are mandatory options."
+   USAGE
+   exit 1
+fi
+
+echo "Hello $NAME. $WISHES. I have been learning shell script."
